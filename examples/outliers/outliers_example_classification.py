Bug fix: squared lengths of partitions after splitting should be stored in long to prevent overflow. Estimate of tree depth implemented as in Isolation Forest.

examples/outliers/outliers_example_classification.py
@@ -10,26 +10,27 @@ from scipy.spatial import distance
 from sklearn.utils import shuffle as sh
 
 # fetch data
-'''X, y = fetch_kddcup99(subset='http', random_state=42, return_X_y=True)
+X, y = fetch_kddcup99(subset='smtp', random_state=42, return_X_y=True)
 X, y = X.astype(np.float32), y.astype('str')
 
 # fix classes
 y_df = pd.DataFrame(y, columns=['class'])
 y_df.loc[y_df['class'] != 'normal.', 'class'] = -1
 y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
-y = y_df.values'''
+y = y_df.values
 
 # smtp take all outliers aside
-'''outliers_indices = np.where(y == -1)[0]
+outliers_indices = np.where(y == -1)[0]
 inliers_indices = np.where(y == 1)[0]
 y_outliers = y[outliers_indices]
 y = y[inliers_indices]
 
 X_outliers = X[outliers_indices]
-X = X[inliers_indices]'''
+X = X[inliers_indices]
 
-# kddcup99 SF subset preprocessing
-'''lb = LabelBinarizer()
+'''# kddcup99 SF subset preprocessing
+X, y = fetch_kddcup99(subset='SF', random_state=42, return_X_y=True)
+lb = LabelBinarizer()
 x1 = lb.fit_transform(X[:, 1].astype(str))
 X = np.c_[X[:, :1], x1, X[:, 2:]]
 y = y.astype('str')
@@ -39,7 +40,8 @@ y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
 y = y_df.values'''
 
 # kddcup99 SA subset preprocessing
-'''lb = LabelBinarizer()
+'''X, y = fetch_kddcup99(subset='SA', random_state=42, return_X_y=True)
+lb = LabelBinarizer()
 x1 = lb.fit_transform(X[:, 1].astype(str))
 x2 = lb.fit_transform(X[:, 2].astype(str))
 x3 = lb.fit_transform(X[:, 3].astype(str))
@@ -51,7 +53,7 @@ y_df.loc[y_df['class'] == 'normal.', 'class'] = 1
 y = y_df.values'''
 
 # shuttle
-dataset = fetch_openml('shuttle')
+'''dataset = fetch_openml('shuttle')
 X = dataset.data
 y = dataset.target
 X, y = sh(X, y, random_state=1)
@@ -62,7 +64,7 @@ s = (y != 4)
 X = X[s, :]
 y = y[s]
 y[(y == 1)] = 1
-y[(y != 1)] = -1
+y[(y != 1)] = -1'''
 
 # forestcover
 '''dataset = fetch_covtype(shuffle=True, random_state=1)
@@ -81,8 +83,8 @@ X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.3, random_state=42)
 
 # smtp concateate outliers to test set
-'''y_test = np.append(y_test, y_outliers)
-X_test = np.append(X_test, X_outliers, axis=0)'''
+y_test = np.append(y_test, y_outliers)
+X_test = np.append(X_test, X_outliers, axis=0)
 
 # scale
 scaler = StandardScaler()
@@ -102,8 +104,8 @@ print(confusion_matrix(y_test, IF_preds))
 # SF
 max_samples = 256
 max_depth = int(np.ceil(np.log2(max(max_samples, 2))))
-SF = SimilarityForestClassifier(sim_function=distance.euclidean, n_estimators=20, random_state=42, bootstrap=True,
-                                max_samples=max_samples, max_depth=15, discriminative_sampling=True)
+SF = SimilarityForestClassifier(sim_function=distance.euclidean, n_estimators=20, random_state=42, bootstrap=False,
+                                max_samples=256, max_depth=max_depth, discriminative_sampling=False)
 SF.fit(X_train, y_train)
 SF_preds = SF.predict_outliers(X_test)
 SF_decision_f = SF.decision_function_outliers(X_test)

simforest/simforest.py
@@ -23,6 +23,20 @@ def gini_index(split_index, y):
     return left_prop * left_gini + (1.0 - left_prop) * right_gini
 
 
+def _h(n):
+    """A function estimating average external path length of Similarity Tree
+        Since Similarity Tree, the same as Isolation tree, has an equivalent structure to Binary Search Tree,
+        the estimation of average h for external node terminations is the same as the unsuccessful search in BST.
+        Parameters
+        ----------
+        n : int, number of objects used for tree construction
+        Returns
+        ----------
+        average external path length : int
+    """
+    return 2 * np.log(n - 1) + np.euler_gamma - 2 * (n - 1) / n
+
+
 class SimilarityTreeClassifier(BaseEstimator, ClassifierMixin):
     """Similarity Tree classifier implementation.
         Similarity Trees are base models used as building blocks for Similarity Forest ensemble.
@@ -534,8 +548,9 @@ class SimilarityTreeClassifier(BaseEstimator, ClassifierMixin):
 
     def row_path_length_(self, x):
         """ Get outlyingness path length of a single data-point.
-            If current node is a leaf, return its depth,
-            if not, traverse down the tree
+            If current node is a leaf, then return its depth, if not, traverse down the tree.
+            If number of objects in external node is more than one, then add an estimate of sub-tree depth,
+            if it was fully grown.
 
             Parameters
             ----------
@@ -546,7 +561,11 @@ class SimilarityTreeClassifier(BaseEstimator, ClassifierMixin):
         """
 
         if self._is_leaf:
-            return self.depth
+            c = 0
+            n = len(self.y_)
+            if n > 1:
+                c = _h(n)
+            return self.depth + c
 
         assert self._p is not None
         assert self._q is not None
@@ -894,16 +913,21 @@ class SimilarityForestClassifier(BaseEstimator, ClassifierMixin):
         return self.classes_[np.argmax(self.predict_proba(X), axis=1)]
 
     def decision_function_outliers(self, X, check_input=True):
-        """Get outlyingness measure for X.
+        """Average anomaly score of X of the base classifiers.
+            The anomaly score of an input sample is computed as the mean anomaly score of the trees in the forest.
+            The measure of normality of an observation given a tree is the depth of the leaf containing this observation
+            which is equivalent to the number of splittings required to isolate this point.
+
             Parameters
             ----------
             X : array-like, shape (n_samples, n_features)
                 The input samples.
-            check_input : bool indicating if input values should be checked or not.
+            check_input : bool indicating if input should be checked or not.
             Returns
             -------
-            outlyingness : ndarray, shape (n_samples,)
-                The outlyingness measure, values are scaled to fit within range between 0 and 1.
+            scores : ndarray, shape (n_samples,)
+                The anomaly score of the input samples. The lower, the more abnormal.
+                Negative scores represent outliers, positive scores represent inliers.
         """
         if check_input:
             # Check if fit had been called
@@ -914,13 +938,17 @@ class SimilarityForestClassifier(BaseEstimator, ClassifierMixin):
 
             X = self._validate_X_predict(X, check_input)
 
-        path_lengths = np.mean([t.path_length_(X, check_input=False) / t.get_depth() for t in self.estimators_], axis=0)
-        n = X.size
-        '''# Isolation Forest scaling factor
-        scaling_factor = 2 * (np.log(n - 1) + np.euler_gamma) - (2 * (n - 1) / n)
+        # Average depth of leaf containing each sample
+        path_lengths = np.mean([t.path_length_(X, check_input=False) for t in self.estimators_], axis=0)
 
-        # Scores are opposite of the anomaly score defined in the original paper.
-        scores = np.array([2 ** (-pl/scaling_factor) for pl in path_lengths])'''
+        # Depths are normalized in the same fashion as in Isolation Forest
+        if self.max_samples is not None:
+            n = min(self.y_.size, self.max_samples)
+        else:
+            n = self.y_.size
+        c = _h(n)
+
+        scores = np.array([1 - 2 ** (-pl/c) for pl in path_lengths])
 
         if self.contamination == 'auto':
             offset_ = 0.5
@@ -933,9 +961,21 @@ class SimilarityForestClassifier(BaseEstimator, ClassifierMixin):
         else:
             raise ValueError('contamination should be set either to \'auto\' or a float value between 0.0 and 0.5')
 
-        return path_lengths - offset_
+        return scores - offset_
 
     def predict_outliers(self, X, check_input=True):
+        """Predict if a particular sample is an outlier or not.
+            Paramteres
+            ----------
+            X : array-like, shape (n_samples, n_features)
+                The input samples.
+            check_input : bool indicating if input should be checked or not.
+            Returns
+            -------
+            is_inlier : array, shape (n_samples,) For each observation, tells whether or not (+1 or -1) it should be
+            considered as an inlier according to the fitted model.
+
+        """
 
         if check_input:
             # Check if fit had been called