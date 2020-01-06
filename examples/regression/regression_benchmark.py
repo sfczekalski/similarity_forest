Small fix in left- and right-hand partitioning calculation.

examples/regression/regression_benchmark.py
@@ -10,7 +10,14 @@ import numpy as np
 from scipy.stats import ttest_ind
 import pandas as pd
 import gc
-from scipy.spatial.distance import sqeuclidean
+
+
+def downcast_dtypes(df):
+    float_cols = [c for c in df if df[c].dtype == "float64"]
+    int_cols = [c for c in df if df[c].dtype in ["int64", "int32"]]
+    df[float_cols] = df[float_cols].astype(np.float32)
+    df[int_cols] = df[int_cols].astype(np.int16)
+    return df
 
 
 def get_hardware_dataset():
@@ -97,9 +104,16 @@ def get_energy_efficiency_cooling():
     return y, X
 
 
-def get_yacht_hydrodynamics_dataset():
-    df = pd.read_csv('../data/yacht_hydrodynamics.csv', header=None)
-    y, X = df.pop(6), df
+def get_who_dataset():
+    df = pd.read_csv('../data/Life Expectancy Data.csv')
+    '''df['Country'] = LabelEncoder().fit_transform(df['Country'])
+    df['Status'] = LabelEncoder().fit_transform(df['Status'])'''
+    df = pd.concat([df, pd.get_dummies(df['Country'])], axis=1)
+    df = pd.concat([df, pd.get_dummies(df['Status'])], axis=1)
+    df.drop(columns=['Country', 'Status'], inplace=True)
+    df.dropna(inplace=True)
+    df = downcast_dtypes(df)
+    y, X = df.pop('Life expectancy '), df
 
     return y, X
 
@@ -111,25 +125,24 @@ params['criterion'] = 'variance'
 params['discriminative_sampling'] = True
 params['max_depth'] = None
 params['n_estimators'] = 100
-params['sim_function'] = sqeuclidean
 
 
 # set experiment properties
-n_iterations = 30
+n_iterations = 10
 
 # store mse for t-test
 rf_mse = np.zeros(shape=(n_iterations,), dtype=np.float32)
 sf_mse = np.zeros(shape=(n_iterations,), dtype=np.float32)
 
 # create experiment
-neptune.create_experiment(name='Regression yacht hydrodynamics variance sqeuclidean',
+neptune.create_experiment(name='Regression WHO life expectancy variance different from mean',
                           params=params,
                           properties={'n_iterations': n_iterations,
-                                      'dataset': 'yacht hydrodynamics'})
+                                      'dataset': 'WHO life expectancy'})
 
 
 # load and prepare data
-y, X = get_yacht_hydrodynamics_dataset()
+y, X = get_who_dataset()
 y = y + np.abs(np.min(y))
 
 X_train, X_test, y_train, y_test = train_test_split(

examples/regression/sf_r_example.py
@@ -40,10 +40,30 @@ print(df.head())
 
 y, X = df.pop('quality'), df'''
 
-df = pd.read_csv('../data/yacht_hydrodynamics.csv', header=None)
-print(df.columns)
-print(df.head())
-'''y, X = get_forest_fires_dataset()
+
+def downcast_dtypes(df):
+    float_cols = [c for c in df if df[c].dtype == "float64"]
+    int_cols = [c for c in df if df[c].dtype in ["int64", "int32"]]
+    df[float_cols] = df[float_cols].astype(np.float32)
+    df[int_cols] = df[int_cols].astype(np.int16)
+    return df
+
+
+def get_who_dataset():
+    df = pd.read_csv('../data/Life Expectancy Data.csv')
+    '''df['Country'] = LabelEncoder().fit_transform(df['Country'])
+    df['Status'] = LabelEncoder().fit_transform(df['Status'])'''
+    df = pd.concat([df, pd.get_dummies(df['Country'])], axis=1)
+    df = pd.concat([df, pd.get_dummies(df['Status'])], axis=1)
+    df.drop(columns=['Country', 'Status'], inplace=True)
+    df.dropna(inplace=True)
+    df = downcast_dtypes(df)
+    y, X = df.pop('Life expectancy '), df
+
+    return y, X
+
+
+y, X = get_who_dataset()
 
 #X = SelectKBest(f_regression, k=8).fit_transform(X, y)
 y = y + np.abs(np.min(y))
@@ -63,12 +83,12 @@ print(f'Random Forest MSE: {mean_squared_error(y_test, rf_pred)}')
 print(f'RF average tree depth: {np.mean([t.get_depth() for t in rf.estimators_])}')
 
 # Fit predict for both classifiers
-sf = SimilarityForestRegressor(criterion='atkinson')
+sf = SimilarityForestRegressor(criterion='theil', n_estimators=100, n_directions=2)
 sf.fit(X_train, y_train)
 sf_pred = sf.predict(X_test)
 print(f'Similarity Forest R2 score: {r2_score(y_test, sf_pred)}')
 print(f'Similarity Forest MSE: {mean_squared_error(y_test, sf_pred)}')
-print(f'SF average tree depth: {[t.get_depth() for t in sf.estimators_]}')'''
+print(f'SF average tree depth: {[t.get_depth() for t in sf.estimators_]}')
 
 # Scale predictions for plotting
 '''sf_pred = (sf_pred - np.min(sf_pred))/np.ptp(sf_pred)

simforest/simforest.py
@@ -1001,7 +1001,7 @@ class SimilarityTreeRegressor(BaseEstimator, RegressorMixin):
             i = np.argmax(np.abs(np.ediff1d(similarities[indices])))
             impurity = weighted_variance(i+1, y[indices])
 
-        split_point = (similarities[indices[i]] + similarities[indices[i + 1]]) / 2
+        split_point = similarities[indices[i]]
 
         if self.plot_splits:
             plot_projection(similarities[indices], p, q, split_point, y[indices],
@@ -1097,18 +1097,13 @@ class SimilarityTreeRegressor(BaseEstimator, RegressorMixin):
 
         # Sample n_direction discriminative directions and find the best one
         best_impurity = np.inf
-        best_split_point = -np.inf
+        best_split_point = None
         best_p = None
         best_q = None
         similarities = []
         for i, j in self._sample_directions(random_state, y, self.n_directions):
 
             impurity, split_point, curr_similarities = self._find_split(X, y, X[i], X[j])
-
-            if split_point == -1:
-                self.is_fitted_ = True
-                self._is_leaf = True
-                return self
 
             if impurity < best_impurity:
                 best_impurity = impurity
@@ -1117,6 +1112,11 @@ class SimilarityTreeRegressor(BaseEstimator, RegressorMixin):
                 best_split_point = split_point
                 similarities = curr_similarities
 
+        if best_split_point is None:
+            self.is_fitted_ = True
+            self._is_leaf = True
+            return self
+
         # if split improves impurity
         if self._impurity - best_impurity > 0.0:
             self._split_point = best_split_point
@@ -1124,9 +1124,10 @@ class SimilarityTreeRegressor(BaseEstimator, RegressorMixin):
             self._q = best_q
             self._similarities = np.array(similarities, dtype=np.float32)
 
+            e = 0.0000001
             # Left- and right-hand side partitioning
-            lhs_idxs = np.nonzero(self._similarities <= self._split_point)[0]
-            rhs_idxs = np.nonzero(self._similarities > self._split_point)[0]
+            lhs_idxs = np.nonzero(self._similarities - self._split_point < e)[0]
+            rhs_idxs = np.nonzero(self._similarities - self._split_point > 0)[0]
 
             if len(lhs_idxs) > 0 and len(rhs_idxs) > 0:
                 self._lhs = SimilarityTreeRegressor(random_state=self.random_state,