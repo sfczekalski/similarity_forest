e0fe6def5be5725f774f1b46c18bf03640ddf9c8
Stanislaw Czekalski <sfczekalski@gmail.com>
2020-01-08 15:31:20 +0100

    Neptune script updated.

--- examples/cluster/clustering_benchmark_real.py
+++ examples/cluster/clustering_benchmark_real.py
@@ -17,14 +17,10 @@ path = '../data/clustering_benchmark/real-world/'
 
 '''
 
-    
-    {'file_name': 'segment.arff',
-     'class_col': 'class',
-     'n_clusters': 7},
-    {'file_name': 'vehicle.arff',
-     'class_col': 'Class',
-     'n_clusters': 4},
-    {'file_name': 'iris.arff',
+'''
+
+datasets = [
+{'file_name': 'iris.arff',
      'class_col': 'class',
      'n_clusters': 3},
     {'file_name': 'vowel.arff',
@@ -38,25 +34,28 @@ path = '../data/clustering_benchmark/real-world/'
      'n_clusters': 20},
     {'file_name': 'ecoli.arff',
      'class_col': 'class',
-     'n_clusters': 8}
-'''
-
-datasets = [
+     'n_clusters': 8},
     {'file_name': 'glass.arff',
      'class_col': 'Class',
      'n_clusters': 7},
     {'file_name': 'sonar.arff',
      'class_col': 'Class',
      'n_clusters': 2},
-    {'file_name': 'wine.arff',
-     'class_col': 'class',
-     'n_clusters': 3},
-    {'file_name': 'balance-scale.arff',
-     'class_col': 'class',
-     'n_clusters': 3},
     {'file_name': 'german.arff',
      'class_col': 'CLASS',
      'n_clusters': 2},
+    {'file_name': 'balance-scale.arff',
+     'class_col': 'class',
+     'n_clusters': 3},
+    {'file_name': 'wine.arff',
+     'class_col': 'class',
+     'n_clusters': 3},
+    {'file_name': 'vehicle.arff',
+     'class_col': 'Class',
+     'n_clusters': 4},
+    {'file_name': 'segment.arff',
+     'class_col': 'class',
+     'n_clusters': 7}
 ]
 
 
@@ -80,42 +79,49 @@ for file_name, class_col, n_clusters in get_datasets(datasets):
     neptune.init('sfczekalski/similarity-forest')
 
     params = dict()
-    params['max_depth'] = 5
+    params['max_depth'] = 10
     params['n_estimators'] = 100
-    params['technique'] = 'ahc'
+    params['technique'] = 'hdbscan'
     params['n_clusters'] = n_clusters
+    params['bootstrap'] = False
 
     # set experiment properties
     n_iterations = 20
     plot = True
 
     # create experiment
-    neptune.create_experiment(name=f'Clustering {file_name} plot no bootstrap',
+    neptune.create_experiment(name=f'Clustering {file_name}',
                               params=params,
                               properties={'n_iterations': n_iterations,
                                           'dataset': file_name,
-                                          'n_clusters': params['n_clusters']})
+                                          'n_clusters': params['n_clusters'],
+                                          'plot': plot})
 
     # store results
     sf_silhouette = np.zeros(shape=(n_iterations,), dtype=np.float32)
     sf_db = np.zeros(shape=(n_iterations,), dtype=np.float32)
 
-    ahc_silhouette = np.zeros(shape=(n_iterations,), dtype=np.float32)
-    ahc_db = np.zeros(shape=(n_iterations,), dtype=np.float32)
+    other_silhouette = np.zeros(shape=(n_iterations,), dtype=np.float32)
+    other_db = np.zeros(shape=(n_iterations,), dtype=np.float32)
 
     for i in range(n_iterations):
         sf = SimilarityForestCluster(**params)
-        sf_clusters = sf.fit_predict(X)
+        try:
+            sf_clusters = sf.fit_predict(X)
+        except ValueError as err:
+            # sometimes all cluster labels are the same
+            print(f'ValueError: {err}')
+            break
         neptune.log_metric('SF Silhouette score', silhouette_score(X, sf_clusters))
         neptune.log_metric('SF Davies Bouldin score', davies_bouldin_score(X, sf_clusters))
         sf_silhouette[i] = silhouette_score(X, sf_clusters)
         sf_db[i] = davies_bouldin_score(X, sf_clusters)
 
-        ahc_clusters = AgglomerativeClustering(n_clusters=params['n_clusters']).fit_predict(X)
-        neptune.log_metric('AHC Silhouette score', silhouette_score(X, ahc_clusters))
-        neptune.log_metric('AHC Davies Bouldin score', davies_bouldin_score(X, ahc_clusters))
-        ahc_silhouette[i] = silhouette_score(X, ahc_clusters)
-        ahc_db[i] = davies_bouldin_score(X, ahc_clusters)
+        other_clusters = KMeans(n_clusters=params['n_clusters']).fit_predict(X)
+        neptune.log_metric('KMeans Silhouette score', silhouette_score(X, other_clusters))
+        neptune.log_metric('KMeans Davies Bouldin score', davies_bouldin_score(X, other_clusters))
+        other_silhouette[i] = silhouette_score(X, other_clusters)
+        other_db[i] = davies_bouldin_score(X, other_clusters)
 
         if plot:
             if X.shape[1] > 2:
@@ -123,8 +129,8 @@ for file_name, class_col, n_clusters in get_datasets(datasets):
             figure, axs = plt.subplots(1, 2, figsize=(10, 5))
             axs[0].scatter(X[:, 0], X[:, 1], c=sf_clusters, cmap='Set1', alpha=0.6)
             axs[0].set_title('SimilarityForestCluster')
-            axs[1].scatter(X[:, 0], X[:, 1], c=ahc_clusters, cmap='Set1', alpha=0.6)
-            axs[1].set_title('AHC')
+            axs[1].scatter(X[:, 0], X[:, 1], c=other_clusters, cmap='Set1', alpha=0.6)
+            axs[1].set_title('KMeans')
             neptune.log_image('Plot', plt.gcf())
             plt.clf()
             plt.close()
@@ -134,16 +140,16 @@ for file_name, class_col, n_clusters in get_datasets(datasets):
     neptune.log_metric('SF mean silhouette', np.mean(sf_silhouette))
     neptune.log_metric('SF mean Davies Bouldin', np.mean(sf_db))
 
-    neptune.log_metric('AHC mean silhouette', np.mean(ahc_silhouette))
-    neptune.log_metric('AHC mean Davies Bouldin', np.mean(ahc_db))
+    neptune.log_metric('KMeans mean silhouette', np.mean(other_silhouette))
+    neptune.log_metric('KMeans mean Davies Bouldin', np.mean(other_db))
 
 
     # compare
-    t, p = ttest_ind(sf_silhouette, ahc_silhouette)
+    t, p = ttest_ind(sf_silhouette, other_silhouette)
     neptune.log_metric('t-stat silhouette', t)
     neptune.log_metric('p-val silhouette', p)
 
-    t, p = ttest_ind(sf_db, ahc_db)
+    t, p = ttest_ind(sf_db, other_db)
     neptune.log_metric('t-stat db', t)
     neptune.log_metric('p-val db', p)
 
