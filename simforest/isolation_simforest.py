8ada3d796cdb44f7bdb7d51b0711f627b5a1f336
Stanislaw Czekalski <sfczekalski@gmail.com>
2020-02-25 14:16:17 +0100

    Docstrings.

--- simforest/isolation_simforest.py
+++ simforest/isolation_simforest.py
@@ -22,6 +22,47 @@ def _h(n):
 
 
 class IsolationSimilarityForest(BaseEstimator):
+    """An algorithm for outlier detection based on Similarity Forest.
+        It borrows the idea of isolating data-points by performing random splits, as in Isolation Forest,
+        but they are not performed on features, but in the same way as in Similarity Forest.
+
+            Parameters
+            ----------
+                random_state : int, RandomState instance or None, optional (default=None)
+                    If int, random_state is the seed used by the random number generator;
+                    If RandomState instance, random_state is the random number generator;
+                    If None, the random number generator is the RandomState instance used
+                    by `np.random`.
+                n_estimators : integer, optional (default=20)
+                    The number of trees in the forest.
+                sim_function : function used to measure similarity between data-points
+                max_depth : integer or None, optional (default=None)
+                    The maximum depth of the tree. If None, then nodes are expanded until
+                    all leaves are pure.
+                max_samples : int or float
+                    size of subsamples used for fitting trees, if int then use number of objects provided, if float then
+                    use fraction of whole sample
+                contamination : string or float (default='auto'), fraction of expected outliers in the data. If auto then
+                    use algorithm criterion described in Isolation Forest paper. Float means fraction of objects that
+                     should be considered outliers.
+                most_different : bool (default = False)
+                    when we don't use strategy of finding split minimizing Gini impurity, we may choose one that finds
+                    most different consecutive elements, and splits at this point. Used for outlier detection.
+
+                Attributes
+                ----------
+                    base_estimator_ : SimilarityIsolationTree
+                        The child estimator template used to create the collection of fitted
+                        sub-estimators.
+                    estimators_ : list of SimilarityTreeClassifiers
+                        The collection of fitted sub-estimators.
+                    is_fitted_ : bool flag indicating whenever fit has been called
+
+                Notes
+                -----
+                    To obtain a deterministic behaviour during
+                    fitting, ``random_state`` has to be fixed.
+            """
 
     def __init__(self,
                  random_state=None,
@@ -40,13 +81,25 @@ class IsolationSimilarityForest(BaseEstimator):
         self.contamination = contamination
 
     def _validate_X_predict(self, X):
-        """Validate X whenever one tries to predict, apply, predict_proba."""
+        """Validate X."""
 
         X = check_array(X)
 
         return X
 
     def fit(self, X, y=None, check_input=True):
+        """Build a forest of trees from the training set X.
+
+            Parameters
+            ----------
+                X : array-like matrix of shape = [n_samples, n_features]
+                    The training data samples.
+                y : None, added to follow Scikit-Learn convention
+                check_input : bool (default=False), allows to skip input validation multiple times.
+            Returns
+            -------
+                self : object.
+                """
         if check_input:
             # Input validation, check it to be a non-empty 2D array containing only finite values
             X = check_array(X)
@@ -55,7 +108,7 @@ class IsolationSimilarityForest(BaseEstimator):
             X = self._validate_X_predict(X)
 
 
-        # Random state
+        # Initialize random number generator instance
         if self.random_state is not None:
             random_state = check_random_state(self.random_state)
         else:
@@ -85,6 +138,7 @@ class IsolationSimilarityForest(BaseEstimator):
 
         self.sample_size = sample_size
 
+        # Build trees
         self.estimators_ = []
         for i in range(self.n_estimators):
 
@@ -101,10 +155,25 @@ class IsolationSimilarityForest(BaseEstimator):
         assert len(self.estimators_) == self.n_estimators, f'Build {len(self.estimators_)} trees, ' \
                                                            f'instead of {self.n_estimators}'
         self.is_fitted_ = True
-
         return self
 
     def decision_function(self, X, check_input=True):
+        """Average anomaly score of X of the base classifiers.
+            The anomaly score of an input sample is computed as the mean anomaly score of the trees in the forest.
+            The measure of normality of an observation given a tree is the depth of the leaf containing this observation
+            which is equivalent to the number of splittings required to isolate this point.
+
+            Parameters
+            ----------
+                X : array-like, shape (n_samples, n_features), the input samples.
+                check_input : bool indicating if input should be checked or not.
+
+            Returns
+            -------
+                scores : ndarray, shape (n_samples,)
+                    The anomaly score of the input samples. The lower, the more abnormal.
+                    Negative scores represent outliers, positive scores represent inliers.
+        """
         if check_input:
             # Check if fit had been called
             check_is_fitted(self, ['is_fitted_'])
@@ -114,7 +183,7 @@ class IsolationSimilarityForest(BaseEstimator):
 
             X = self._validate_X_predict(X)
 
-        # Average depth at which a sample lies
+        # Average depth at which a sample lies over all trees
         mean_path_lengths = np.mean([t.path_length_(X, check_input=False) for t in self.estimators_], axis=0)
         assert len(mean_path_lengths) == len(X)
 
@@ -136,6 +205,17 @@ class IsolationSimilarityForest(BaseEstimator):
         return scores - offset_
 
     def predict(self, X, check_input=True):
+        """Predict if a particular sample is an outlier or not.
+            Paramteres
+            ----------
+                X : array-like, shape (n_samples, n_features)
+                    The input samples.
+                check_input : bool indicating if input should be checked or not.
+            Returns
+            -------
+                is_inlier : array, shape (n_samples,) For each observation, tell whether or not (+1 or -1) it should be
+                considered as an inlier according to the fitted model.
+        """
         if check_input:
             # Check if fit had been called
             check_is_fitted(self, ['is_fitted_'])
@@ -154,6 +234,38 @@ class IsolationSimilarityForest(BaseEstimator):
 
 
 class IsolationSimilarityTree:
+    """Unsupervised Similarity Tree measuring outlyingness score.
+        Isolation Similarity Trees are base models used as building blocks for Isolation Similarity Forest ensemble.
+            Parameters
+            ----------
+            random_state : int, random numbers generator seed
+            sim_function : function used to measure similarity between data-points
+            max_depth : integer or None, optional (default=None)
+                The maximum depth of the tree. If None, then nodes are expanded until
+                all leaves are pure.
+            depth : int depth of the tree count
+            most_different : bool (default = False)
+                when we don't use strategy of finding split minimizing Gini impurity, we may choose one that finds
+                most different consecutive elements, and splits at this point. Used for outlier detection.
+
+            Attributes
+            ----------
+            is_fitted_ : bool flag indicating whenever fit has been called
+
+            _lhs : SimilarityTreeClassifier current node's left child node
+            _rhs : SimilarityTreeClassifier current node's right child node
+            _p : first data-point used for drawing split direction in the current node
+            _q : second data-point used for drawing split direction in the current node
+            _similarities :
+                ndarray of similarity values between two datapoints used for splitting and rest of training datapoints
+            _split_point = float similarity value decision boundary
+            _value = class value probabilities for current node, estimated based on training set
+            _is_leaf :
+                bool indicating if current node is a leaf, because it is pure or stopping createrion
+                has been reached (depth == max_depth)
+            n_ : int, number of data-points in current node
+
+    """
 
     def __init__(self,
                  sim_function=dot_product,
@@ -213,22 +325,24 @@ class IsolationSimilarityTree:
         self._q = None
         self._similarities = []
         self._split_point = -np.inf
-        self._value = None
-        self._is_leaf = False
         self.is_fitted_ = False
         self.n_ = X.shape[0]
 
-        # one data-point, or multiple copies of the same data-point
+        # If there is only one data-point, or multiple copies of the same data-point, stop growing a tree.
         if X.shape[0] == 1 or len(np.unique(X, axis=0)) == 1:
             self._is_leaf = True
             return self
 
+        # Max depth reached
         if self.max_depth is not None:
             if self.depth == self.max_depth:
                 self._is_leaf = True
                 return self
 
+        # Sample data-points used to draw split direction
         self._p, self._q = self.sample_directions(X, random_state)
+
+        # Project all data-points onto the split direction
         self._split_point, self._similarities = self.find_split(X, random_state)
 
         # Left- and right-hand side partitioning
@@ -252,17 +366,26 @@ class IsolationSimilarityTree:
                              'even though the split had been found')
 
         self.is_fitted_ = True
-
         return self
 
     def _validate_X_predict(self, X):
-        """Validate X whenever one tries to predict, apply, predict_proba."""
+        """Validate X."""
 
         X = check_array(X)
 
         return X
 
     def path_length_(self, X, check_input=True):
+        """Get path length for instances of X.
+            Parameters
+            ----------
+                X : array-like, shape (n_samples, n_features)
+                    The input samples.
+            Returns
+            -------
+                path_length : ndarray, shape (n_samples,)
+                    The path_length for instances of X, according to a single tree.
+        """
         if check_input:
             # Check if fit had been called
             check_is_fitted(self, ['is_fitted_'])
@@ -275,6 +398,12 @@ class IsolationSimilarityTree:
         return np.array([self.row_path_length_(x.reshape(1, -1)) for x in X])
 
     def depth_estimate(self):
+        """Based on depth of not-fully grown tree and number of data-points in current node,
+            estimate depth of fully grown tree.
+                Returns
+                -------
+                    depth : int, estimate of fully grown tree.
+        """
         c = 0
         n = self.n_
         if n > 1:
@@ -282,6 +411,18 @@ class IsolationSimilarityTree:
         return self.depth + c
 
     def row_path_length_(self, x):
+        """ Get outlyingness path length of a single data-point.
+            If current node is a leaf, then return its depth, if not, traverse down the tree.
+            If number of objects in external node is more than one, then add an estimate of sub-tree depth,
+            if it was fully grown.
+
+            Parameters
+            ----------
+            x : a data-point
+            Returns
+            -------
+            data-point's path length, according to single a tree.
+        """
         if self._is_leaf:
             return self.depth_estimate()
 
